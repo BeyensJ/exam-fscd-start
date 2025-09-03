35d19e574f24886795f3bb2e27c91f34e49eef07
Jitse Beyens <u0157807@thomasmore.be>
2025-09-03 11:48:05 +0200

    Fix INSERT statement for dogs in dogs.sql

--- backend/dogs.sql
+++ backend/dogs.sql
@@ -16,10 +16,11 @@ CREATE TABLE IF NOT EXISTS dogsCatalog.dogs (
 
 INSERT INTO dogsCatalog.dogs (
     dogName, birthDate, personality,
-    surrenderYear, goodWithKids, goodWithAnimals
+    surrenderYear, goodWithKids, goodWithAnimals, dogImage
 ) VALUES
 ('Blacky', '2023-12-13', 'Happy', 2024, true, true, 'https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg'),
 ('Francine', '2020-05-05', 'Bitey', 2020, false, false, 'https://images.pexels.com/photos/8473664/pexels-photo-8473664.jpeg'),
 ('Franky', '2021-10-08', 'Nervous', 2023, true, false, 'https://images.pexels.com/photos/33629326/pexels-photo-33629326.jpeg'),
 ('Ezra', '2022-02-20', 'Hyperactive', 2023, true, false, 'https://images.pexels.com/photos/32383207/pexels-photo-32383207.jpeg'),
+
 ('Bob', '2019-03-28', 'Droopy', 2021, false, true, 'https://images.pexels.com/photos/28668008/pexels-photo-28668008.jpeg')
